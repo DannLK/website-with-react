ddde41afbd3b6f95d011ac32b12d54cdb7aaaf2d
Daniel Lok <daniel.lok@ericsson.com>
2019-05-15 14:52:02 -0400

    test commit2

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -3,3 +3,6 @@ import Grid from "@material-ui/core/Grid";
 import TextField from "@material-ui/core/TextField";
 import * as contentful from "contentful";
 import Projects from "../../Projects";
+
+const SPACE_ID = "";
+const ACCESS_TOKEN = "";
